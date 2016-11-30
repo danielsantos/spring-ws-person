1902dbf908d1406e382fdf35e2ad9ecf770f441b
Daniel dos Santos Rocha <daniel.rocha@riocard.com>
2016-11-30 13:58:52 -0200

    first commit

---
+++ src/main/java/com/danielrocha/SpringWsPersonApplication.java
@@ -0,0 +1,12 @@
+package com.danielrocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWsPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWsPersonApplication.class, args);
+	}
+}
---
+++ src/main/java/com/danielrocha/repository/PersonRepository.java
@@ -0,0 +1,54 @@
+package com.danielrocha.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import com.danielrocha.model.Country;
+
+@Component
+public class PersonRepository {
+	private static final List<Country> countries = new ArrayList<Country>();
+
+	@PostConstruct
+	public void initData() {
+		Country spain = new Country();
+		spain.setName("Spain");
+		spain.setCapital("Madrid");
+		spain.setPopulation(46704314);
+
+		countries.add(spain);
+
+		Country poland = new Country();
+		poland.setName("Poland");
+		poland.setCapital("Warsaw");
+		poland.setPopulation(38186860);
+
+		countries.add(poland);
+
+		Country uk = new Country();
+		uk.setName("United Kingdom");
+		uk.setCapital("London");
+		uk.setPopulation(63705000);
+
+		countries.add(uk);
+	}
+
+	public Country findCountry(String name) {
+		Assert.notNull(name);
+
+		Country result = null;
+
+		for (Country country : countries) {
+			if (name.equals(country.getName())) {
+				result = country;
+			}
+		}
+
+		return result;
+	}
+}
